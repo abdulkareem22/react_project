773b54e9a8f1cc11efd1959b0f714d172e07cbfb
asp <asp@aptech.com>
2024-08-24 12:26:37 +0500

    react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+    let name = "xyz"
+    return(
+        <>
+        <h1>Hello {name}</h1>
+        </>
+    )
+}
+
+export default  App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDom from 'react-dom/client';
+import App from './App';
+
+const AppRoot = ReactDom.createRoot(document.getElementById("root"));
+
+AppRoot.render(<App/>)
